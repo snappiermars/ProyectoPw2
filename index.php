d4181b29ce41067e72d7e3d283ffcdbd004de10c
snappiermars <snappiermars848@gmail.com>
2023-05-21 22:32:16 -0600

    Integracion de bootstrap para el diseño y que el sitio sea responsive

--- index.php
+++ index.php
@@ -3,11 +3,16 @@
 
                 <h1>Login de sistema</h1>
                 <form   method="POST"  action="logica/loguear.php">
-                    <input type="text" name="numCuenta" placeholder="Numero de Cuenta" />
-                        <br />
-                    <input type="text" name="clave" placeholder="Contraseña" />
-                        <br />
-                    <button type="submit">Inicar Sesion</button>
+                    <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">NUMERO DE CUENTA</label>
+                    <input type="text" class="form-control" name="numCuenta" placeholder="Numero de Cuenta" />
+                    </div>
+                    <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">CONTRASEÑA</label>
+                    <input type="text" class="form-control" name="clave" placeholder="Contraseña" />
+                    </div>
+                    <button type="submit" class="btn btn-primary">Inicar Sesion</button>
+                    
                 </form> 
             
             
